5da52f49c1efa42c2a86866524593f29cd94728f
appsmambo <quintanilla.peru@gmail.com>
2015-03-11 10:28:36 -0500

    primer commit

---
+++ app/routes.php
@@ -0,0 +1,3 @@
+<?php
+
+Route::get('/', array('as' => 'index', 'uses' => 'HomeController@index'));
---
+++ app/views/home.blade.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<title>Tendencias Ripley Otoño-Invierno 2015 #tueliges</title>
+		<meta name="description" content="Masculine, bohemio, militar...descubre las nuevas tendencias que te trae Ripley este Otoño Invierno 2015">
+		<link rel="canonical" href="{{url()}}/">
+		<!--meta property="fb:app_id" content="242776895908289">
+		<meta property="og:title" content="Blog Ripley">
+		<meta property="og:type" content="blog">
+		<meta property="og:url" content="http://blogripley.com/">
+		<meta property="og:image" content="http://blogripley.com/images/facebook.jpg">
+		<meta property="og:site_name" content="Blog Ripley">
+		<meta property="fb:admins" content="100008010128050">
+		<meta property="og:description" content="Masculine, bohemio, militar...descubre las nuevas tendencias que te trae Ripley este Otoño Invierno 2015">
+		<meta name="twitter:card" content="summary">
+		<meta name="twitter:site" content="@RipleyenPeru">
+		<meta name="twitter:description" content="Masculine, bohemio, militar...descubre las nuevas tendencias que te trae Ripley este Otoño Invierno 2015"-->
+		
+		<link rel="icon" href="{{url()}}/favicon.ico" type="image/x-icon">
+		<link rel="stylesheet" type="text/css" href="{{url()}}/scripts/home/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="{{url()}}/css/home.css">
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<script src="{{url()}}/scripts/queryloader2.min.js"></script>
+		<script type="text/javascript">
+			window.addEventListener('DOMContentLoaded', function () {
+				new QueryLoader2(document.querySelector("body"), {
+					barColor: "#fff",
+					backgroundColor: "#181a20",
+					percentage: true,
+					barHeight: 1,
+					minimumTime: 200,
+					fadeOutTime: 1000
+				});
+			});
+		</script>
+		<script src="{{url()}}/scripts/jquery.min.js"></script>
+		<script src="{{url()}}/scripts/home/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<nav class="navbar navbar-default visible-xs">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="{{url()}}">Tendencias Ripley 2015 #tueliges</a>
+				</div>
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="{{url()}}">Home <span class="sr-only">(current)</span></a></li>
+						<li><a href="#">Tendencias</a></li>
+						<li><a href="#">Video</a></li>
+						<li><a href="#">Concurso</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Síguenos <span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="#">Twitter</a></li>
+								<li><a href="#">Facebook</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+		
+		<div class="jumbotron vertical-center">
+			<section class="container-fluid children">
+				<div class="row">
+					<div class="col-sm-2">
+						<img src="{{url()}}/images/home/elige-ser-tu-misma.jpg" class="img-responsive center-block" alt="">
+					</div>
+					<div class="col-sm-2">
+						<a href="#">
+							<img src="{{url()}}/images/home/extra-normal.jpg" class="img-responsive center-block" alt="">
+						</a>
+					</div>
+					<div class="col-sm-1">
+						<img src="{{url()}}/images/home/tendecias-2015.jpg" class="img-responsive center-block" alt="">
+					</div>
+					<div class="col-sm-2 hidden-xs container-menu">
+						<img src="{{url()}}/images/home/menu.png" class="img-responsive center-block" alt="">
+						<div>
+							<p>
+								<a href="#">
+									HOME
+								</a>
+								<br>
+								<a href="#">
+									TENDENCIAS
+								</a>
+								<br>
+								<a href="#">
+									VIDEO
+								</a>
+								<br>
+								<a href="#">
+									CONCURSO
+								</a>
+							</p>
+							<hr>
+							<p class="redes">
+								<a href="#">
+									<img src="{{url()}}/images/twitter.png" alt="">
+								</a>
+								<a href="#">
+									<img src="{{url()}}/images/facebook.png" alt="">
+								</a>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+						<a href="#">
+							<img src="{{url()}}/images/home/tendecias-2015-video.jpg" class="img-responsive center-block" alt="">
+						</a>
+					</div>
+					<div class="col-sm-1">
+						<a href="#">
+							<img src="{{url()}}/images/home/must.jpg" class="img-responsive center-block" alt="">
+						</a>
+						<img src="{{url()}}/images/home/tu-eliges-1.jpg" class="img-responsive hidden-xs center-block" alt="">
+					</div>
+					<div class="col-sm-2">
+						<a href="#">
+							<img src="{{url()}}/images/home/participa-y-gana.jpg" class="img-responsive center-block" alt="">
+						</a>
+						<img src="{{url()}}/images/home/tu-eliges-2.jpg" class="img-responsive hidden-xs center-block" alt="">
+					</div>
+				</div>
+				<div class="row visible-xs">
+					<div class="col-xs-12">
+						<img src="{{url()}}/images/home/tu-eliges.jpg" class="img-responsive center-block" alt="">
+					</div>
+				</div>
+			</section>
+		</div>
+	</body>
+</html>
