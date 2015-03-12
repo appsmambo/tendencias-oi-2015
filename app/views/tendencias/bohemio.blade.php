2bfb3af24dac657f9323257c97c9fa240835bede
Alex <alex1820m@gmail.com>
2015-03-11 22:46:55 -0500

    Ajustes

---
+++ app/views/tendencias/bohemio.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.tendencias')
+@section('content')
+<div class="row">
+    <div class="col-sm-1">
+        <p class="info" style="font-size:30px;line-height:40px;">
+            <img src="{{url()}}/images/bohemio/logo.jpg" alt="" class="img-responsive">
+            <br>
+            DESEO EXCESIVO DE VESTIR LO QUE TENGAS A LA MANO CUANDO DESPIERTAS PORQUE ERES UNA PERSONA NORMAL Y SOLO QUIERES TENER UNA VIDA NORMAL.
+        </p>
+    </div>
+    <div class="col-sm-2">
+        <img src="{{url()}}/images/bohemio/1.jpg" alt="" class="center-block img-responsive">
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-2">
+        <img src="{{url()}}/images/bohemio/2.jpg" alt="" class="center-block img-responsive">
+    </div>
+    <div class="col-sm-2">
+        <img src="{{url()}}/images/bohemio/3.jpg" alt="" class="center-block img-responsive">
+    </div>
+</div>
+@stop
---
+++ app/views/tendencias/militar.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.tendencias')
+@section('content')
+<div class="row">
+    <div class="col-sm-1">
+        <p class="info" style="font-size:30px;line-height:36px;">
+            <img src="{{url()}}/images/militar/logo.jpg" alt="" class="img-responsive">
+            <br>
+            DESEO EXCESIVO DE VESTIR LO QUE TENGAS A LA MANO CUANDO DESPIERTAS PORQUE ERES UNA PERSONA NORMAL Y SOLO QUIERES TENER UNA VIDA NORMAL.
+        </p>
+        <img src="{{url()}}/images/militar/1.jpg" alt="" class="center-block img-responsive">
+    </div>
+    <div class="col-sm-2">
+        <img src="{{url()}}/images/militar/2.jpg" alt="" class="center-block img-responsive">
+    </div>
+    <div class="col-sm-1">
+        <img src="{{url()}}/images/militar/3.jpg" alt="" class="center-block img-responsive">
+    </div>
+</div>
+@stop
