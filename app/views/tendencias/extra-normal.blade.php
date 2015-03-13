14c4eb699a198e915c8260a5f1651978a916cfa4
appsmambo <quintanilla.peru@gmail.com>
2015-03-13 00:31:21 -0500

    ajustes

--- app/views/tendencias/extra-normal.blade.php
+++ app/views/tendencias/extra-normal.blade.php
@@ -9,14 +9,17 @@
 		</p>
 		<br>
 		<div class="bloque-prenda">
+			<div style="display:none">
+				Navigata Chompa Carola 16307856 S/.79.90 Pantalón Caracas 16323481 S/.129
+			</div>
 			<p>
-				<a target="_blank" href="#">
+				<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http://tueliges.com.pe/extra-normal/cacharel-chompa-carola-pantalon-caracas">
 					<img src="{{url()}}/images/facebook-blanco.png" alt="">
 				</a>
-				<a target="_blank" href="#">
+				<a target="_blank" href="https://twitter.com/home?status=Me%20fascin%C3%B3%20este%20look%20Extra%20Normal%20de%20la%20nueva%20colecci%C3%B3n%20Oto%C3%B1o-Invierno%202015%20de%20Ripley%20%23tueliges%20http://bit.ly/1E9Dvtr">
 					<img src="{{url()}}/images/twitter-blanco.png" alt="">
 				</a>
-				<a target="_blank" href="#">
+				<a target="_blank" href="https://pinterest.com/pin/create/button/?url=http://tueliges.com.pe/extra-normal/cacharel-chompa-carola-pantalon-caracas&media=http://tueliges.com.pe/images/extra-normal/1.jpg&description=Me%20fascin%C3%B3%20este%20look%20Extra%20Normal%20de%20la%20nueva%20colecci%C3%B3n%20Oto%C3%B1o-Invierno%202015%20de%20Ripley%20%23tueliges">
 					<img src="{{url()}}/images/pinterest-blanco.png" alt="">
 				</a>
 			</p>
@@ -27,14 +30,17 @@
 		<div class="row">
 			<div class="col-sm-2">
 				<div class="bloque-prenda">
+					<div style="display:none">
+						Marquis Blusa Pope 16399959 S/.79.90 Chompa Carina 16469756 S/.99.90 Blazer Mila 16388025 S/.169 Pantalón Jogger 16354532 S/.129
+					</div>
 					<p>
-						<a target="_blank" href="#">
+						<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http://tueliges.com.pe/extra-normal/marquis-blusa-pope-chompa-carina-blazer-mila-pantalon-jogger">
 							<img src="{{url()}}/images/facebook-interna.png" alt="">
 						</a>
-						<a target="_blank" href="#">
+						<a target="_blank" href="https://twitter.com/home?status=Me%20fascin%C3%B3%20este%20look%20Extra%20Normal%20de%20la%20nueva%20colecci%C3%B3n%20Oto%C3%B1o-Invierno%202015%20de%20Ripley%20%23tueliges%20http://bit.ly/1HOf48w">
 							<img src="{{url()}}/images/twitter-interna.png" alt="">
 						</a>
-						<a target="_blank" href="#">
+						<a target="_blank" href="https://pinterest.com/pin/create/button/?url=http://tueliges.com.pe/extra-normal/marquis-blusa-pope-chompa-carina-blazer-mila-pantalon-jogger&media=http://tueliges.com.pe/images/extra-normal/2.jpg&description=Me%20fascin%C3%B3%20este%20look%20Extra%20Normal%20de%20la%20nueva%20colecci%C3%B3n%20Oto%C3%B1o-Invierno%202015%20de%20Ripley%20%23tueliges">
 							<img src="{{url()}}/images/pinterest-interna.png" alt="">
 						</a>
 					</p>
@@ -43,14 +49,17 @@
 			</div>
 			<div class="col-sm-2">
 				<div class="bloque-prenda">
+					<div style="display:none">
+						Marquis Polerón Besbre 16347849 S/.79.90 Pantalón Off 16740564 S/.99.90
+					</div>
 					<p>
-						<a target="_blank" href="#">
+						<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http://tueliges.com.pe/extra-normal/marquis-poleron-besbre-pantalon-off">
 							<img src="{{url()}}/images/facebook-blanco.png" alt="">
 						</a>
-						<a target="_blank" href="#">
+						<a target="_blank" href="https://twitter.com/home?status=Me%20fascin%C3%B3%20este%20look%20Extra%20Normal%20de%20la%20nueva%20colecci%C3%B3n%20Oto%C3%B1o-Invierno%202015%20de%20Ripley%20%23tueliges%20http://bit.ly/1GKMXcL">
 							<img src="{{url()}}/images/twitter-blanco.png" alt="">
 						</a>
-						<a target="_blank" href="#">
+						<a target="_blank" href="https://pinterest.com/pin/create/button/?url=http://tueliges.com.pe/extra-normal/marquis-poleron-besbre-pantalon-off&media=http://tueliges.com.pe/images/extra-normal/3.jpg&description=Me%20fascin%C3%B3%20este%20look%20Extra%20Normal%20de%20la%20nueva%20colecci%C3%B3n%20Oto%C3%B1o-Invierno%202015%20de%20Ripley%20%23tueliges">
 							<img src="{{url()}}/images/pinterest-blanco.png" alt="">
 						</a>
 					</p>
@@ -61,14 +70,17 @@
 		<div class="row">
 			<div class="col-sm-4">
 				<div class="bloque-prenda">
+					<div style="display:none">
+						IZQUIERDA: Marquis Polo Grey Marl 16430361 S/.99.90 Blazer Longline 16291665 S/.299<br>DERECHA: Marquis Polo Cami 16314475 S/.59.90 Chompa Maxi 16396045 S/.69.90 Pantalón Gris 16706002 S/.99.90
+					</div>
 					<p>
-						<a target="_blank" href="#">
+						<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http://tueliges.com.pe/extra-normal/marquis-polo-grey-marl-blazer-longline">
 							<img src="{{url()}}/images/facebook-interna.png" alt="">
 						</a>
-						<a target="_blank" href="#">
+						<a target="_blank" href="https://twitter.com/home?status=Me%20fascin%C3%B3%20este%20look%20Extra%20Normal%20de%20la%20nueva%20colecci%C3%B3n%20Oto%C3%B1o-Invierno%202015%20de%20Ripley%20%23tueliges%20http://bit.ly/1FgHgS7">
 							<img src="{{url()}}/images/twitter-interna.png" alt="">
 						</a>
-						<a target="_blank" href="#">
+						<a target="_blank" href="https://pinterest.com/pin/create/button/?url=http://tueliges.com.pe/extra-normal/marquis-polo-grey-marl-blazer-longline&media=http://tueliges.com.pe/images/extra-normal/6.jpg&description=Me%20fascin%C3%B3%20este%20look%20Extra%20Normal%20de%20la%20nueva%20colecci%C3%B3n%20Oto%C3%B1o-Invierno%202015%20de%20Ripley%20%23tueliges">
 							<img src="{{url()}}/images/pinterest-interna.png" alt="">
 						</a>
 					</p>
@@ -79,14 +91,17 @@
 	</div>
 	<div class="col-sm-1">
 		<div class="bloque-prenda">
+			<div style="display:none">
+				Navigata Blusa Beck 16613317 S/.79.90 Jean Boyfriend 16289948 S/.129
+			</div>
 			<p>
-				<a target="_blank" href="#">
+				<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http://tueliges.com.pe/extra-normal/navigata-blusa-beck-jean-boyfriend">
 					<img src="{{url()}}/images/facebook-interna.png" alt="">
 				</a>
-				<a target="_blank" href="#">
+				<a target="_blank" href="https://twitter.com/home?status=Me%20fascin%C3%B3%20este%20look%20Extra%20Normal%20de%20la%20nueva%20colecci%C3%B3n%20Oto%C3%B1o-Invierno%202015%20de%20Ripley%20%23tueliges%20http://bit.ly/1ArEd39">
 					<img src="{{url()}}/images/twitter-interna.png" alt="">
 				</a>
-				<a target="_blank" href="#">
+				<a target="_blank" href="https://pinterest.com/pin/create/button/?url=http://tueliges.com.pe/extra-normal/navigata-blusa-beck-jean-boyfriend&media=http://tueliges.com.pe/images/extra-normal/4.jpg&description=Me%20fascin%C3%B3%20este%20look%20Extra%20Normal%20de%20la%20nueva%20colecci%C3%B3n%20Oto%C3%B1o-Invierno%202015%20de%20Ripley%20%23tueliges">
 					<img src="{{url()}}/images/pinterest-interna.png" alt="">
 				</a>
 			</p>
@@ -97,13 +112,13 @@
 				Marquis Mochila Muriel S/.139
 			</div>
 			<p>
-				<a target="_blank" href="#">
+				<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http://tueliges.com.pe/extra-normal/marquis-mochila-muriel">
 					<img src="{{url()}}/images/facebook-interna.png" alt="">
 				</a>
-				<a target="_blank" href="#">
+				<a target="_blank" href="https://twitter.com/home?status=Me%20fascin%C3%B3%20este%20look%20Extra%20Normal%20de%20la%20nueva%20colecci%C3%B3n%20Oto%C3%B1o-Invierno%202015%20de%20Ripley%20%23tueliges%20http://bit.ly/1EEmX1X">
 					<img src="{{url()}}/images/twitter-interna.png" alt="">
 				</a>
-				<a target="_blank" href="#">
+				<a target="_blank" href="https://pinterest.com/pin/create/button/?url=http://tueliges.com.pe/extra-normal/marquis-mochila-muriel&media=http://tueliges.com.pe/images/extra-normal/5.jpg&description=Me%20fascin%C3%B3%20este%20look%20Extra%20Normal%20de%20la%20nueva%20colecci%C3%B3n%20Oto%C3%B1o-Invierno%202015%20de%20Ripley%20%23tueliges">
 					<img src="{{url()}}/images/pinterest-interna.png" alt="">
 				</a>
 			</p>
